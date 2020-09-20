75e962d3d906ede659d9d6743f9dd6ca5133cf66
Link <2454546080@qq.com>
2020-09-20 23:02:55 +0800

    基础redis配置读取

---
+++ Config/conf.go
@@ -0,0 +1,41 @@
+package Config
+
+import (
+	"io/ioutil"
+	"log"
+	"gopkg.in/yaml.v2"
+)
+
+//redis配置
+var configFile []byte
+
+const path string = "config-dev.yaml"
+
+type Config struct {
+	Redis Redis `json:"redis"`
+}
+
+type Redis struct {
+	Address string `json:"address"`
+	Network string `json:"network"`
+	Password string `json:"password"`
+	MaxIdle string `json:"maxidle"`
+	MaxActive string `json:"maxactive"`
+	Timeout string `json:"timeout"`
+}
+
+//获取配置信息
+func GetConfig()(config Config,err error)  {
+	err = yaml.Unmarshal(configFile,&config)
+	return
+}
+
+//初始时自动调用加载redis信息到比特流
+func init()  {
+	var err error
+	configFile,err = ioutil.ReadFile(path)
+	if err!=nil{
+		log.Fatal("redis init file open err : %v",err)
+	}
+
+}
---
+++ Redis/redisInit.go
@@ -0,0 +1,6 @@
+package Redis
+
+import (
+
+)
+
